81ebabfea90dd48052db5ca6d6f25114df0abda9
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-11-07 22:47:23 +0000

    adding resp

---
+++ resp.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func tmp() {
+	input := "$5\r\nAhmed\r\n"
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	b, _ := reader.ReadByte()
+	if b != '$' {
+		fmt.Println("Invalid type, expecting bulk strings only")
+		os.Exit(1)
+	}
+
+	size, _ := reader.ReadByte()
+
+	strSize, _ := strconv.ParseInt(string(size), 10, 64)
+	reader.ReadByte()
+	reader.ReadByte()
+
+	name := make([]byte, strSize)
+	reader.Read(name)
+	fmt.Println(string(name))
+}
